52fd1633165a28525ec02d95bb8cf4029e090b2f
Diegostoles1978 <diegoarlesvc@hotmail.com>
2021-09-27 22:12:59 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ATT.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
+
+
+
+
